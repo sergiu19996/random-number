e6257bbf07640098dfe33d1d46f5654abe34e69e
sergiu19996 Porime <s.porime@yahoo.com>
2024-03-02 14:58:16 +0000

    adding terminal to the game

--- guess-number.py
+++ guess-number.py
@@ -1,5 +1,17 @@
-import random
- def guess(x):
+from simple_term_menu import TerminalMenu
+
+def main():
+    options = ["learn the rules", "play the game ", "exit"]
+    terminal_menu = TerminalMenu(options)
+    menu_entry_index = terminal_menu.show()
+    print(f"You have selected {options[menu_entry_index]}!")
+
+if __name__ == "__main__":
+    main()
+
+
+
+def guess(x):
     random_number = random.randint(1, x)
     guess = 0
     while guess != random_number:
